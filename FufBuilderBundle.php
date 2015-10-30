a572785f9a7e2b73df92feac9f6482476b82bcc9
Christoph Martel <cm@fuf.de>
2015-10-30 11:04:51 +0100

    reinitialize from task

---
+++ FufBuilderBundle.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Generic comannd line helper for symfonoy 2 projects.
+ */
+namespace Fuf\BuilderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FufBuilderBundle extends Bundle
+{
+}
